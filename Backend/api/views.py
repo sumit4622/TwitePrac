47eb9f8f500d6cc055f059ce233a079184311dcd
sumitraay2022@gmail.com <sumitraay2022@gmail.com>
2025-06-03 22:46:16 +0545

    working on jija template

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,6 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import templates
+from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import get_object_or_404, redirect
 # Create your views here.
 
 def sumit(request):
-    return HttpResponse("<h1>hello this is Sumit</h1>")
+    return render(request, 'index.html')
+
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request, 'Tweet_list.html', {'tweets': tweets})
+
+def tweet_create(request):
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+        
+    else:
+        form = TweetForm()
+    return render(request, 'TweetForm.html', {'form' : form})
+
+def tweet_edit(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk = tweet_id, user = request.user )
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES, instance = tweet)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm( instance = tweet)
+    return render(request, 'TweetForm.html', {'form': form})
+
+def tweet_delete(request,tweet_id):
+    tweet = get_object_or_404(Tweet, pk = tweet_id, user= request.user)
+    if request.method == 'POST':
+        tweet.delete()
+        return redirect('tweet_list')
+    return render(request, 'TweetDelete.html', {'tweet' : tweet})
